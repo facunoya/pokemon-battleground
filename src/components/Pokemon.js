4b8f890728648cdb636db7c296d766d1dda5b7d6
Facundo Noya <facunoya@hotmail.com>
2023-12-02 18:05:34 -0300

    se llevo la carpeta assets al public

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -6,6 +6,7 @@ const Pokemon = ({ user, pokemon }) => {
         <div className={user === "user" ? classes.userPokemonWrapper : classes.enemyPokemonWrapper}>
             <LifeBar currentHP={pokemon.currentHP} maxHP={pokemon.maxHP}></LifeBar>
             <div className={user === "user" ? classes.userPokemon : classes.enemyPokemon}>
+                <img src="./assets/img/pokemon2.png" className={classes.pokemonImage} />
             </div>
         </div>
     )
